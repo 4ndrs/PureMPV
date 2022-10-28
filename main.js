0765c540e141dcdc983cfa392124d58261cc5bf5
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-10-28 07:02:47 +0200

    Rename PureMPV.js to purempv.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 // Copyright (c) 2022 4ndrs <andres.degozaru@gmail.com>
 // SPDX-License-Identifier: MIT
 // eslint-disable-next-line
-var PureMPV = require("./lib/PureMPV");
+var PureMPV = require("./lib/purempv");
