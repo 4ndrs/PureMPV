cad8d51329aad3767595bbf9c85bd76acf35d445
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-10-28 08:18:30 +0200

    Add getTimePosition to util.js

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,8 @@
 // Copyright (c) 2022 4ndrs <andres.degozaru@gmail.com>
 // SPDX-License-Identifier: MIT
 
+import { DEBUG } from "./env";
+
 /* global mp */
 
 export function copyToSelection(text, selection) {
@@ -16,6 +18,13 @@ export function copyToSelection(text, selection) {
 
 export function getFilePath() {
   return mp.get_property("path");
+}
+
+export function getTimePosition() {
+  // Returns the current timestamp in the format HH:MM:SS
+  const timePos = mp.get_property("time-pos");
+  DEBUG && print(`DEBUG: TIME-POS: ${timePos}`);
+  return new Date(timePos * 1000).toISOString().substring(11, 23);
 }
 
 export function printMessage(message) {
