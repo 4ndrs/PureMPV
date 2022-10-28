ee78ee992dfa6963b8612db9d278990c12112b74
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-10-28 06:30:10 +0200

    Convert video and mouse to properties classes
    Removed the initialization of the video's properties from PureBox's
    constructor and added DEBUG from env.js to print debugging information.
    
    PureBox's interface is now working with this commit.

--- src/purebox.js
+++ src/purebox.js
@@ -3,31 +3,44 @@
 
 /* global mp */
 
+import { VideoProperties, MouseProperties } from "./properties";
+import { DEBUG } from "./env";
+
 export default class PureBox {
   constructor() {
-    this.video = {
-      width: mp.get_property("width"),
-      height: mp.get_property("height"),
-    };
-
+    this.video = new VideoProperties();
+    this.mouse = new MouseProperties();
     this.pid = mp.utils.getpid();
   }
 
   getCrop() {
-    [this.x, this.y] = mp.get_property_native("mouse-pos");
+    this.video.getProperties();
+    this.mouse.getProperties();
+
+    DEBUG &&
+      print(
+        "DEBUG: PUREBOX INITIAL VALUES:",
+        this.video.width.toString(),
+        this.video.height.toString(),
+        this.mouse.x.toString(),
+        this.mouse.y.toString()
+      );
 
     const pureBoxProcess = mp.command_native({
       name: "subprocess",
       args: [
         "purebox",
         this.pid.toString(),
-        this.x.toString(),
-        this.y.toString(),
+        this.mouse.x.toString(),
+        this.mouse.y.toString(),
         this.video.width.toString(),
         this.video.height.toString(),
       ],
       capture_stdout: true,
     });
+
+    DEBUG && print(`DEBUG: PUREBOX STATUS: ${pureBoxProcess.status}`);
+    DEBUG && print(`DEBUG: PUREBOX RESULTS: ${pureBoxProcess.stdout}`);
 
     if (pureBoxProcess.status == 0) {
       return pureBoxProcess.stdout.split(", ");
