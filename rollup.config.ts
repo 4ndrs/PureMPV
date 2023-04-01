c00c13fb71dc5bef7759be278b8ee90e7abd4f03
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-04-01 19:42:12 +0200

    chore: rename purempv.ts to index.ts

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "rollup";
+
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 
 const plugins = [typescript(), terser()];
 
 export default defineConfig({
-  input: "src/purempv.ts",
+  input: "src/index.ts",
   output: {
     file: "main.js",
     format: "cjs",
